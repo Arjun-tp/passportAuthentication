9943939f9dffe639b2327ea61169f37d750f9a17
Arjun <arjun@blockgemini.com>
2020-03-12 15:00:12 +0400

    create user api

--- authBackend/server/src/api/services/sql/auth.service.js
+++ authBackend/server/src/api/services/sql/auth.service.js
@@ -9,50 +9,6 @@ const model = "[user.model]";
 // const uploadfile = require('./../../utils/uploadFiles')
 const authCodeTimeOut = 900000; //6hours = 21600000
 
-exports.fetchUserByEmail = async obj => {
-  const methodName = "[fetchUserByEmail]";
-  try {
-    const criteria = {
-      where: {
-        email: obj.email
-      }
-    };
-    return await User.findOne(criteria);
-  } catch (error) {
-    logger.error(model, methodName, error);
-    return false;
-  }
-};
-
-exports.fetchUserByActivationCode = async obj => {
-  const methodName = "[fetchUserByActivationCode]";
-  try {
-    const criteria = {
-      where: {
-        activationCode: obj.activationCode
-      }
-    };
-    return await User.findOne(criteria);
-  } catch (error) {
-    logger.error(model, methodName, error);
-    return false;
-  }
-};
-
-exports.fetchUserByActivationCodeStashed = async (obj) => {
-  const methodName = "[fetchUserByActivationCodeStashed]";
-  try {
-    const criteria = {
-      where: {
-        activationCodeStashed: obj.id
-      }
-    };
-    return await User.findOne(criteria);
-  } catch (error) {
-    logger.error(model, methodName, error);
-    return false;
-  }
-};
 
 exports.createUser = async query => {
   const methodName = "[createUser]";
@@ -71,48 +27,3 @@ exports.createUser = async query => {
   }
 };
 
-exports.resetPassword = async function(body, params, passwordInDb) {
-  const methodName = "[resetPassword]";
-  try {
-    /*let compare = await bcrypt.compareSync(
-      body.currentPassword,
-      passwordInDb.password
-    );
-
-    if (compare) {
-      const updateQuery = {
-        password: bcrypt.hashSync(body.newPassword, salt)
-      };
-      const criteria = {
-        where: {
-          uuid: params.id
-        }
-      };
-      return await User.update(updateQuery, criteria);
-    } else {
-      logger.error(model, methodName, "Password Compare failed");
-      return false;
-    }*/
-  } catch (error) {
-    logger.error(model, methodName, error);
-    return false;
-  }
-};
-
-exports.resetForgotPassword = async body => {
-  const methodName = "[resetForgotPassword]";
-  try {
-    /*const updateQuery = {
-      password: bcrypt.hashSync(body.password, salt)
-    };
-    const criteria = {
-      where: {
-        uuid: body.id
-      }
-    };
-    return await User.update(updateQuery, criteria);*/
-  } catch (error) {
-    logger.error(model, methodName, error);
-    return false;
-  }
-};
