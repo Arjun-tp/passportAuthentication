79ab44a85bdc9e5d888b59a293c953026661bf6e
Arjun <arjun@blockgemini.com>
2020-03-12 14:29:11 +0400

    Backend Changes

--- authBackend/server/src/api/models/sql/user.model.js
+++ authBackend/server/src/api/models/sql/user.model.js
@@ -11,24 +11,12 @@ module.exports = function (sequelize, DataTypes) {
             defaultValue: DataTypes.UUIDV4,
             unique: true,
         },
-        firstName: {
+        fullName: {
             type: DataTypes.STRING,
             trim: true,
             validate: {
                 len: [1, 128],
             },
-        },
-        lastName: {
-            type: DataTypes.STRING,
-            trim: true,
-            validate: {
-                len: [1, 128],
-            },
-        },
-        accountId:{
-            type: DataTypes.STRING,
-            trim: true,
-            unique: true,
         },
         gender: {
             // type: DataTypes.ENUM,
@@ -36,10 +24,6 @@ module.exports = function (sequelize, DataTypes) {
             // trim: true,
             type: DataTypes.STRING,
             allowNull:true
-        },
-        residentialAddress: {
-            type: DataTypes.STRING,
-            trim: true,
         },
         email: {
             type: DataTypes.STRING,
@@ -51,19 +35,6 @@ module.exports = function (sequelize, DataTypes) {
                 is: /^\S+@\S+\.\S+$/,
             },
         },
-        receivePromotionalEmails: {
-            type: DataTypes.BOOLEAN,
-            defaultValue: false,
-            notEmpty: true,
-        },
-        dob: {
-            type: DataTypes.DATE,
-            allowNull:true
-            // trim: true,
-            // validate: {
-            //     notEmpty: true,
-            // },
-        },
         password: {
             type: DataTypes.STRING,
             allowNull: false,
@@ -72,113 +43,10 @@ module.exports = function (sequelize, DataTypes) {
                 len: [8, 128],
             },
         },
-        kycStatus: {
-            type: DataTypes.ENUM,
-            values: Enum.kycStatus,
-            // validate: {
-            //     notEmpty: true,
-            // },
-        },
-        isActive: {
-            type: DataTypes.BOOLEAN,
-            defaultValue: true,
-            notEmpty: true,
-        },
-        nationality: {
-            type: DataTypes.STRING,
-            trim: true,
-            validate: {
-                len: [1, 128],
-            }
-        },
         phoneNo: {
             type: DataTypes.STRING,
             trim: true,
             allowNull:true
-            // validate: {
-            //     len: [1, 128],
-            // }
-        },
-        countryOfResidence: {
-            type: DataTypes.STRING,
-            trim: true,
-            validate: {
-                len: [1, 128],
-            }
-        },
-        hideInvestmentActivity: {
-            type: DataTypes.BOOLEAN,
-            defaultValue: true,
-        },
-        profileImage: {
-            type: DataTypes.STRING,
-            trim: true,
-            allowNull:true
-        },
-        identityDocumentType: {
-            type: DataTypes.STRING,
-            trim: true,
-            allowNull:true
-        },
-        IdentityDocumentImage: {
-            type: DataTypes.STRING,
-            trim: true,
-            allowNull:true
-        },
-        IdentityDocumentNumber: {
-            type: DataTypes.STRING,
-            trim: true,
-            allowNull:true
-        },
-        userPhotoWithId: {
-            type: DataTypes.STRING,
-            trim: true,
-            allowNull:true
-        },
-        isEmailVerified: {
-            type: DataTypes.BOOLEAN,
-            defaultValue: false,
-        },
-        activationCode: {
-            type: DataTypes.STRING,
-            unique: true,
-        },
-        activationCodeStashed: {
-            type: DataTypes.STRING,
-            trim: true,
-
-        },
-        provider: {
-            type: DataTypes.STRING
-        },
-        bioDesc: {
-            type: DataTypes.STRING,
-            trim: true,
-        },
-        tAndCAgree: {
-            type: DataTypes.BOOLEAN,
-            defaultValue: false,
-            notEmpty: true
-        },
-        disabled: {
-            type: DataTypes.BOOLEAN,
-            defaultValue: false,
-            notEmpty: true
-        },
-        isInvestor: {
-            type: DataTypes.BOOLEAN,
-            defaultValue: false,
-            notEmpty: true
-        },
-        isIssuer: {
-            type: DataTypes.BOOLEAN,
-            defaultValue: false,
-            notEmpty: true
-        },
-        twoFaEnabled: {
-            type: DataTypes.BOOLEAN,
-            defaultValue: false,
-            notEmpty: true
         },
     }, {
         indexes: [{
@@ -187,31 +55,6 @@ module.exports = function (sequelize, DataTypes) {
         }],
     });
 
-
-    /**
-   * Pre-save hooks
-   */
-
-    /*User.afterCreate(async (user, options) => {
-        const methodName = '[afterCreate]';
-        user.dataValues.pgUserId = user.dataValues.id;
-        try {
-            const userData = await (new mongoUser(user.dataValues)).save();
-            redisCache.set('user', userData.pgUserId, userData);
-            return userData;
-        } catch (err) {
-            logger.error('[UserModel]', methodName, err);
-            return err;
-        }
-    });*/
-    /*User.associate = (models) => {
-        console.log('models===>', models)
-        User.belongsTo(models.CompanyKYC, {
-            foreignKey: {allowNull: false},
-            as: 'user',
-            onDelete: 'CASCADE'
-        })
-    };*/
 
     return User;
 };
--- authBackend/server/src/config/sequelize.js
+++ authBackend/server/src/config/sequelize.js
@@ -14,51 +14,17 @@ const {
 // Open sequelize connection
 if (mysql.enabled) {
     const sequelize = new Sequelize(mysql.db, mysql.username, mysql.password, {
-    // the sql dialect of the database
-    // currently supported: 'mysql', 'sqlite', 'mysql', 'mssql'
         dialect: 'mysql',
-
-        // custom host; default: localhost
         host: mysql.host,
-
-        // custom port; default: dialect default
         port: mysql.port,
-
-        // custom protocol; default: 'tcp'
-        // mysqls only, useful for Heroku
-        // protocol: null,
-
-        // disable logging; default: console.log
         logging: false,
-
-        // you can also pass any dialect options to the underlying dialect library
-        // - default is empty
-        // - currently supported: 'mysql', 'mysql', 'mssql'
         dialectOptions: {
-            // socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock',
             supportBigNumbers: true,
             bigNumberStrings: true,
         },
 
-        // the storage engine for sqlite
-        // - default ':memory:'
-        // storage: 'path/to/database.sqlite',
-
-        // disable inserting undefined values as NULL
-        // - default: false
         omitNull: true,
-
-        // a flag for using a native library or not.
-        // in the case of 'pg' -- set this to true will allow SSL support
-        // - default: false
         native: true,
-
-        // Specify options, which are used when sequelize.define is called.
-        // The following example:
-        //   define: { timestamps: false }
-        // is basically the same as:
-        //   sequelize.define(name, attributes, { timestamps: false })
-        // so defining the timestamps for each model will be not necessary
         define: {
             underscored: false,
             freezeTableName: true,
@@ -68,18 +34,9 @@ if (mysql.enabled) {
             },
             timestamps: true,
         },
-
-        // similar for sync: you can define this to always force sync for models
         sync: {
             force: true,
         },
-
-        // pool configuration used to pool database connections
-        // last code
-        // max: 5,
-        // min: 0,
-        // idle: 20000,
-        // acquire: 60000
         pool: {
             max: 50,
             min: 1,
@@ -88,10 +45,6 @@ if (mysql.enabled) {
             evict: 100000,
             handleDisconnects: true,
         },
-
-    // isolation level of each transaction
-    // defaults to dialect default
-    // isolationLevel: Transaction.ISOLATION_LEVELS.REPEATABLE_READ
     });
 
     sequelizeTransforms(sequelize);
